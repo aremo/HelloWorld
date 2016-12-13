4357ab383c64834e87d6f6857ef331f135a811b8
Arian Kompani Zare <arian.k.zare@gmail.com>
2016-12-13 19:35:52 +0100

    Utworzenie projektu

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by aremo on 12/13/2016.
+ */
+class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hejka Ho!");
+        System.out.println(255);
+    }
+}
