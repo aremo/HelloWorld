50a1a524f8f2c7846c9617d678c4002a678d0555
Arian Kompani Zare <arian.k.zare@gmail.com>
2016-12-13 19:38:58 +0100

    Tłumaczenie projektu na polski

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,7 @@
 class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hejka Ho!");
+        System.out.println("Witaj świecie!");
         System.out.println(255);
     }
 }
